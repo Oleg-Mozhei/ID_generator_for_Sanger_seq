8c933910b48f3d208d0e7d0b5ccc5153a48eb1cc
Oleg Mozhey <omozhei@gmail.com>
2021-10-06 15:03:23 +0200

    evacuation

--- LabelsGenerator.java
+++ LabelsGenerator.java
@@ -1,43 +1,47 @@
 import java.util.Random;
+import java.sql.*;
+import java.nio.file.*;
+import java.io.*;
+import java.util.*;
 
 public class LabelsGenerator{
   static Random r = new Random();
+  final Connection conn;
+
+  public LabelsGenerator(){
+  try{
+        conn = getConnection();
+    } catch(Exception ex){
+        ex.printStackTrace();
+    }
+    }
 
   public static void main(String[] args){
+  LabelsGenerator generator = new LabelsGenerator();
+
   System.out.println("hello world!");
-	System.out.println("changes from little server!");
 
-  LabelsGenerator l = new LabelsGenerator();     //connect to add during initialization
-  String label = l.generateLabel();
-  System.out.println(label);
-
+  System.out.print(getFirstSymbol());
+  System.out.print(getFirstSymbol());
+  System.out.print(getFirstSymbol());
+  System.out.print(getFirstSymbol());
+  System.out.print(getFirstSymbol());
+  System.out.print(getFirstSymbol());
+  System.out.print(getFirstSymbol());
+  System.out.println();
 
   //LU8WXNG
 
   }
 
-  public String generateNewLabel(){
-    return null;
-  }
 
-  public String generateLabel(){
-    String label = "";
-    for (int i = 0; i < 7; i++){
-      label +=getAllowedSymbol();
-    }
-    //System.out.println(label);
-    return label;
-  }
-
-
-  public char getAllowedSymbol(){
+  public static char getFirstSymbol(){
     int a = 0;
     while(true){
       //System.out.println("I want to get 49-57 and 65-90");
-      a = r.nextInt(35);    //0-34
-      a+=49;                //49 - 83 
-      if (a > 57){a+=7;}    //49-57 & 65-90
-      if (a!= 73 || a!=76 || a!= 79){     //exclude 73, 76 and 79
+      a = r.nextInt(35) + 49;   //49 - 83
+      if (a > 57){a+=8;}
+      if (a!= 73 || a!=76 || a!= 79){
         break;
       }
     }
@@ -46,4 +50,18 @@ public class LabelsGenerator{
 
 
   }
+
+
+  private Connection getConnection() throws SQLException, IOException{
+
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("database.properties"))){
+            props.load(in);
+        }
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        return DriverManager.getConnection(url, username, password);
+    }
 }
